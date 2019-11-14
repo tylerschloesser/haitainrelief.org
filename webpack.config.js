f083d89bf8a840facb9958d12b7c78be41fd0ad2
Tyler 🐱 <tylerschloesser@gmail.com>
2019-11-14 08:06:13 -0800

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+  ],
+}
