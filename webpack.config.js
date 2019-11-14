ed3a204599a63522c19d5cf330b1b64c616260cf
Tyler 🐱 <tylerschloesser@gmail.com>
2019-11-14 08:24:25 -0800

    add mustache

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const page = yaml.safeLoad(fs.readFileSync('./page.yaml', 'utf8'))
+console.log(page)
 
 module.exports = {
   entry: './index.js',
@@ -6,9 +11,18 @@ module.exports = {
     path: __dirname + '/dist',
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.mustache/,
+        loader: 'mustache-loader',
+        options: { render: page },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html'
+      template: './index.mustache'
     }),
   ],
 }
